bef43db02d6771edca8c8c2216564286eb680676
Imam <imamsetiawan110@gmail.com>
2022-06-14 09:34:28 +0700

    fix

--- api/create.php
+++ api/create.php
@@ -7,17 +7,31 @@
 
 	include_once '../config/database.php';
 	// include_once '../class/Pegawai.php';
+	include_once '../class/Mahasiswa.php';
 
 	$database = new Database();
 	$db = $database->getKoneksi();
 	// $peg= new Pegawai($db);
+	$mhs = new Mahasiswa($db);
 	$data = json_decode(file_get_contents("php://input"));
 	
+	$mhs->ktp = $data->ktp;
+	$mhs->nama = $data->nama;
+	$mhs->email = $data->email;
+	$mhs->tmplahir = $data->tmplahir;
+	$mhs->tgllahir = $data->tgllahir;
+
 	// $peg->nama = $data->nama;
 	// $peg->email = $data->email;
 	// $peg->umur = $data->umur;
 	// $peg->jabatan = $data->jabatan;
 	// $peg->created = date('Y-m-d H:i:s');
+
+	if($mhs->tambahMahasiswa()){
+		echo json_encode('Tambah rekaman mahasiswa successfully.');
+	} else{
+		echo json_encode('Terjadi kesalah data tidak direkam.');
+	}
 	
 	// if($peg->tambahPegawai()){
 	// 	echo json_encode('Tambah rekaman pegawai successfully.');
--- api/read.php
+++ api/read.php
@@ -4,36 +4,44 @@
 
 	include_once '../config/database.php';
 	// include_once '../class/Pegawai.php';
+	include_once '../class/Mahasiswa.php';
+
 
 	$database = new Database();
 	$db = $database->getKoneksi();
 	
 	// $peg = new Pegawai($db);
+	$mhs = new Mahasiswa($db);
 	// $stmt = $peg->bacaPegawai();
+	$stmt = $mhs->bacaMahasiswa();
 	// $jumlahPeg = $stmt->rowCount();
+	$jumlahMhs = $stmt->rowCount();
 	// echo json_encode($jumlahPeg);
-	// if ($jumlahPeg > 0) {
-	// 	$pegArr = array();
-	// 	$pegArr["body"] = array();
-	// 	$pegArr["jumlahPeg"] = $jumlahPeg;
-	// 	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-	// 		extract($row);
-	// 		$e = array(
-	// 		"id" => $id,
-	// 		"nama" => $nama,
-	// 		"email" => $email,
-	// 		"umur" => $umur,
-	// 		"jabatan" => $jabatan,
-	// 		"created" => $created
-	// 		);
-	// 	array_push($pegArr["body"], $e);
-	// 	}
-	// 	echo json_encode($pegArr);
-	// }
-	// else{
-	// 	http_response_code(404);
-	// 	echo json_encode(
-	// 		array("message" => "Rekaman tidak.")
-	// 	);
-	// }
+	echo json_encode($jumlahMhs);
+
+	if ($jumlahMhs > 0) {
+		$mhsArr = array();
+		$mhsArr["body"] = array();
+		$mhsArr["jumlahMhs"] = $jumlahMhs;
+		while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+			extract($row);
+			$e = array(
+			"id" => $id,
+			"ktp" => $ktp,
+			"nama" => $nama,
+			"email" => $email,
+			"tmplahir" => $tmplahir,
+			"tgllahir" => $tgllahir,
+			"created" => $created
+			);
+		array_push($mhsArr["body"], $e);
+		}
+		echo json_encode($mhsArr);
+	}
+	else{
+		http_response_code(404);
+		echo json_encode(
+			array("message" => "Rekaman tidak.")
+		);
+	}
 ?>
